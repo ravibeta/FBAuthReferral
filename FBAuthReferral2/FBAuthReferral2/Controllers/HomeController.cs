a42cc9af4810da84a78dcab4d098c87a68695e10
Ravi Rajamani <ravibeta@hotmail.com>
2011-12-10 08:26:02 -0800

    refactoring code

--- FBAuthReferral2/FBAuthReferral2/Controllers/HomeController.cs
+++ FBAuthReferral2/FBAuthReferral2/Controllers/HomeController.cs
@@ -24,6 +24,20 @@ namespace FBAuthReferral2.Controllers
             return View();
         }
 
+        private string GetWebResponse(string url)
+        {
+            var uri = new Uri(url);
+            string responseStr;
+            var response = Redirect(url);
+            WebRequest req = WebRequest.Create(uri);
+            WebResponse resp = req.GetResponse();
+            using (var stream = resp.GetResponseStream())
+            {
+                StreamReader sr = new StreamReader(stream);
+                responseStr = sr.ReadToEnd();
+            }
+            return responseStr;
+        }
         public ActionResult Access()
         {
             try
@@ -32,31 +46,14 @@ namespace FBAuthReferral2.Controllers
                 var code = this.HttpContext.Request.QueryString["code"];
                 code = @"AQACu9NM3ARgebtKR2OBWVxVmwppP1sWwADT0v-llA6a0f-7gf4mwcyYvB0Hntk0IzNPFD1vVT0DGqGEAi7dJ6hUmY1eYGmpMN-rHo4j7oUZz41_TeF1o_3H1zag3MtOb1K25AEuk-dVv0iCBaSUx7Dp7I5IjtuMVulh3iw8pGD1PHxq4emwJpPDO2vId4cPvkw#_=_";
                 var url = @"https://graph.facebook.com/oauth/access_token?client_id=198552693558851&redirect_uri=http://localhost:50713/Home/&client_secret=2bbb5ea24cc13b4b8e4dfaa19d05e328&code=" + code;
-                var uri = new Uri(url);
-                string responseStr;
-                var response = Redirect(url);
-                WebRequest req = WebRequest.Create(uri);
-                WebResponse resp = req.GetResponse();
-                using (var stream = resp.GetResponseStream())
-                {
-                    StreamReader sr = new StreamReader(stream);
-                    responseStr = sr.ReadToEnd();
-                }
+                string responseStr = GetWebResponse(url);
                 var accessToken = responseStr.Contains("&") ? responseStr.Substring(responseStr.IndexOf("=") + 1, responseStr.IndexOf("&") - responseStr.IndexOf("=") - 1) :
                     responseStr.Substring(responseStr.IndexOf("=") + 1, responseStr.Length - responseStr.IndexOf("=") - 1);
                 var graphUrl = @"https://graph.facebook.com/me/checkins?access_token=" + accessToken;
-                var uri2 = new Uri(graphUrl);
-                WebRequest req2 = WebRequest.Create(uri2);
-                WebResponse resp2 = req2.GetResponse();
-                string checkins;
+                string checkins = GetWebResponse(graphUrl);
                 string zip = null;
-                using (var stream = resp2.GetResponseStream())
-                {
-                    StreamReader sr = new StreamReader(stream);
-                    checkins = sr.ReadToEnd();
-                    zip = checkins.Contains("zip") ? 
-                        checkins.Substring(responseStr.IndexOf(@"zip"":""")+6, 5) : null;
-                }
+                zip = checkins.Contains("zip") ? 
+                    checkins.Substring(responseStr.IndexOf(@"zip"":""")+6, 5) : null;
                 if (zip != null)
                 {
                     var bingUrl = @"http://api.bing.net/xml.aspx?Appid=27F556AEB7C7501091E5B878C273F7FE12AFA91D&query=Nordstrom%20<zip>&sources=web";
@@ -87,14 +84,7 @@ namespace FBAuthReferral2.Controllers
                 else
                 {
                     var graphUrl4 = @"https://graph.facebook.com/me?access_token=" + accessToken;
-                    var uri4 = new Uri(graphUrl4);
-                    WebRequest req4 = WebRequest.Create(uri4);
-                    WebResponse resp4 = req4.GetResponse();
-                    using (var stream = resp4.GetResponseStream())
-                    {
-                        StreamReader sr = new StreamReader(stream);
-                        responseStr = sr.ReadToEnd();
-                    }
+                    responseStr = GetWebResponse(graphUrl4);
                 }
                 ViewBag.Message = responseStr + "\r\nIf you enable facebook checkin, this application can try to find a Nordstorm store near you";
             }
