8d12a1f15d1a4e61d30612d5e2782c1068848019
Ravi Rajamani <ravibeta@hotmail.com>
2012-04-05 08:55:13 -0700

    touchup

--- FBAuthReferral2/FBAuthReferral2/Controllers/HomeController.cs
+++ FBAuthReferral2/FBAuthReferral2/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.IO;
 using System.Xml;
 using System.Xml.Linq;
+using System.Text.RegularExpressions;
 
 namespace FBAuthReferral2.Controllers
 {
@@ -43,6 +44,7 @@ namespace FBAuthReferral2.Controllers
             try
             {
                 //TODO: CSRF protection
+                // use RegularExpressionValidator
                 var code = this.HttpContext.Request.QueryString["code"];
                 code = @"AQACu9NM3ARgebtKR2OBWVxVmwppP1sWwADT0v-llA6a0f-7gf4mwcyYvB0Hntk0IzNPFD1vVT0DGqGEAi7dJ6hUmY1eYGmpMN-rHo4j7oUZz41_TeF1o_3H1zag3MtOb1K25AEuk-dVv0iCBaSUx7Dp7I5IjtuMVulh3iw8pGD1PHxq4emwJpPDO2vId4cPvkw#_=_";
                 var url = @"https://graph.facebook.com/oauth/access_token?client_id=198552693558851&redirect_uri=http://localhost:50713/Home/&client_secret=2bbb5ea24cc13b4b8e4dfaa19d05e328&code=" + code;
